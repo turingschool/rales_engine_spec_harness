6bfaebc869ea2a942263b06166b303627f675149
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-07-28 05:43:25 -0600

    added time based finder assertions

--- test/endpoints/customers_test.rb
+++ test/endpoints/customers_test.rb
@@ -31,20 +31,24 @@ class CustomerApiTest < ApiTest
     by_first_name = load_data("/api/v1/customers/find?first_name=#{customer['first_name']}")
     by_last_name = load_data("/api/v1/customers/find?last_name=#{customer['last_name']}")
     by_full_name = load_data("/api/v1/customers/find?name=#{full_name}")
+    by_created_at = load_data("/api/v1/customers/find?created_at=#{customer['created_at']}")
+    by_updated_at = load_data("/api/v1/customers/find?updated_at=#{customer['updated_at']}")
 
 
     assert_equal customer, by_id
     assert_equal customer, by_first_name
     assert_equal customer, by_last_name
     assert_equal customer, by_full_name
+    assert_equal customer, by_created_at
+    assert_equal customer, by_updated_at
   end
 
   def test_it_can_find_all_instances_by_any_attribute
-    customer = {"id"=>"599",
+    customer = {"id"=>599,
                 "first_name"=>"Anibal",
                 "last_name"=>"Boyle",
-                "created_at"=>"2012-03-27T14:54:27.000Z",
-                "updated_at"=>"2012-03-27T14:54:27.000Z"
+                "created_at"=>"2012-03-27T14:56:35.000Z",
+                "updated_at"=>"2012-03-27T14:56:35.000Z"
     }
     full_name = "Anibal Boyle"
 
@@ -52,11 +56,16 @@ class CustomerApiTest < ApiTest
     by_first_name = load_data("/api/v1/customers/find_all?first_name=#{customer['first_name']}")
     by_last_name = load_data("/api/v1/customers/find_all?last_name=#{customer['last_name']}")
     by_full_name = load_data("/api/v1/customers/find_all?name=#{full_name}")
+    by_created_at = load_data("/api/v1/customers/find_all?created_at=#{customer['created_at']}")
+    by_updated_at = load_data("/api/v1/customers/find_all?updated_at=#{customer['updated_at']}")
 
     assert_equal 1, by_id.count
     assert_equal 2, by_first_name.count
     assert_equal 6, by_last_name.count
     assert_equal 1, by_full_name.count
+    assert_equal customer, by_full_name.first
+    assert_equal 4, by_created_at.count
+    assert_equal 4, by_updated_at.count
 
     by_last_name.each do |cust|
       assert_equal customer['last_name'], cust['last_name']
@@ -64,6 +73,14 @@ class CustomerApiTest < ApiTest
 
     by_first_name.each do |cust|
       assert_equal customer['first_name'], cust['first_name']
+    end
+
+    by_created_at.each do |cust|
+      assert_equal customer['created_at'], cust['created_at']
+    end
+
+    by_updated_at.each do |cust|
+      assert_equal customer['updated_at'], cust['updated_at']
     end
   end
 end
