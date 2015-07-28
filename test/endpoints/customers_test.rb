718c46ea82a0ce54e9da9f2d0d194d3a07a606fd
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-07-28 03:31:03 -0600

    First #find endpoint test complete.
    The others should come quickly.

--- test/endpoints/customers_test.rb
+++ test/endpoints/customers_test.rb
@@ -17,4 +17,29 @@ class CustomerApiTest < ApiTest
       assert_class_equal "customer", cust
     end
   end
+
+  def test_it_can_find_first_instance_by_any_attribute
+    customer = {"id"=>75,
+                "first_name"=>"Shayne",
+                "last_name"=>"Kirlin",
+                "created_at"=>"2012-03-27T14:54:27.000Z",
+                "updated_at"=>"2012-03-27T14:54:27.000Z"
+    }
+    full_name = "Shayne-Kirlin"
+
+    by_id = load_data("/api/v1/customers/find?id=#{customer['id']}")
+    by_first_name = load_data("/api/v1/customers/find?first_name=#{customer['first_name']}")
+    by_last_name = load_data("/api/v1/customers/find?last_name=#{customer['last_name']}")
+    by_full_name = load_data("/api/v1/customers/find?name=#{full_name}")
+
+
+    assert_equal customer, by_id
+    assert_equal customer, by_first_name
+    assert_equal customer, by_last_name
+    assert_equal customer, by_full_name
+  end
+
+  def test_it_can_find_all_instances_by_any_attribute
+    skip
+  end
 end
