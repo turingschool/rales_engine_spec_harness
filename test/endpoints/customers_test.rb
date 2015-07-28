b7c8c55ca676251e9432b2e17fd5d6a61d8820f8
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-07-28 04:24:45 -0600

    Added find_all finder to customer.
    I skipped the extensions which really just avoid the query parameters--a
    solution I don't even think we'll want the students to try.

--- test/endpoints/customers_test.rb
+++ test/endpoints/customers_test.rb
@@ -40,6 +40,30 @@ class CustomerApiTest < ApiTest
   end
 
   def test_it_can_find_all_instances_by_any_attribute
-    skip
+    customer = {"id"=>"599",
+                "first_name"=>"Anibal",
+                "last_name"=>"Boyle",
+                "created_at"=>"2012-03-27T14:54:27.000Z",
+                "updated_at"=>"2012-03-27T14:54:27.000Z"
+    }
+    full_name = "Anibal-Boyle"
+
+    by_id = load_data("/api/v1/customers/find_all?id=#{customer['id']}")
+    by_first_name = load_data("/api/v1/customers/find_all?first_name=#{customer['first_name']}")
+    by_last_name = load_data("/api/v1/customers/find_all?last_name=#{customer['last_name']}")
+    by_full_name = load_data("/api/v1/customers/find_all?name=#{full_name}")
+
+    assert_equal 1, by_id.count
+    assert_equal 2, by_first_name.count
+    assert_equal 6, by_last_name.count
+    assert_equal 1, by_full_name.count
+
+    by_last_name.each do |cust|
+      assert_equal customer['last_name'], cust['last_name']
+    end
+
+    by_first_name.each do |cust|
+      assert_equal customer['first_name'], cust['first_name']
+    end
   end
 end
--- test/extensions/customer_extension_test.rb
+++ test/extensions/customer_extension_test.rb
@@ -3,6 +3,7 @@ require "./test/test_helper"
 class CustomerApiExtensionTest < ApiTest
   # Implement Slug
   def test_loads_customers_by_first_name
+    skip
     customer = load_data("/api/v1/customers/Anibal").sort_by {|d| d["id"]}
 
     assert_equal "Boyle",   customer[0]["last_name"]
@@ -10,6 +11,7 @@ class CustomerApiExtensionTest < ApiTest
   end
 
   def test_loads_customers_by_last_name
+    skip
     customer = load_data("/api/v1/customers/Kohler").sort_by {|d| d["id"]}
 
     assert_equal "Aaron",    customer[0]["first_name"]
@@ -19,6 +21,7 @@ class CustomerApiExtensionTest < ApiTest
   end
 
   def test_loads_customers_by_full_name
+    skip
     customer = load_data("/api/v1/customers/Tyree_Weissnat").first
 
     assert_equal 540,        customer["id"]
