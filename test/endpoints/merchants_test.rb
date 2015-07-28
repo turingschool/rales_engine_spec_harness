4f2b576609528dae170325cf56c173ae69a98bb2
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-07-28 05:16:46 -0600

    Finished Merchant finders.
    These tests aren't as interesting because id and name
    are unique for this dataset. I couldn't get the created_at
    working.

--- test/endpoints/merchants_test.rb
+++ test/endpoints/merchants_test.rb
@@ -16,4 +16,36 @@ class MerchantsApiTest < ApiTest
       assert_class_equal "merchant", merch
     end
   end
+
+  def test_it_can_find_first_instance_by_any_attribute
+    merchant = {"id"=>64,
+                "name"=>"Koepp, Waelchi and Donnelly",
+                "created_at"=>"2012-03-27T14:54:05.000Z",
+                "updated_at"=>"2012-03-27T14:54:05.000Z"
+    }
+
+    by_id = load_data("/api/v1/merchants/find?id=#{merchant['id']}")
+    by_name = load_data("/api/v1/merchants/find?name=#{merchant['name']}")
+
+
+    assert_equal merchant, by_id
+    assert_equal merchant, by_name
+  end
+
+  def test_it_can_find_all_instances_by_any_attribute
+    merchant = {"id"=>33,
+                "name"=>"Quitzon and Sons",
+                "created_at"=>"2012-03-27T14:54:02.000Z",
+                "updated_at"=>"2012-03-27T14:54:02.000Z"
+    }
+
+    by_id = load_data("/api/v1/merchants/find_all?id=#{merchant['id']}")
+    by_name = load_data("/api/v1/merchants/find_all?name=#{merchant['name']}")
+
+
+    assert_equal 1, by_id.count
+    assert_equal 1, by_id.count
+    assert_equal merchant, by_id.first
+    assert_equal merchant, by_name.first
+  end
 end
