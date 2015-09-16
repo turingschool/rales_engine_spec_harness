7acc5107e73e687ef7584430a9891e64622c5d5e
Jorge Téllez <novohispano@gmail.com>
2015-09-16 15:13:51 -0600

    Fix BI expectations

--- test/business_logic/merchant_business_logic_test.rb
+++ test/business_logic/merchant_business_logic_test.rb
@@ -9,8 +9,8 @@ class SingleMerchantApiBusinessLogicTest < ApiTest
     favorite_customer_one = load_data("/api/v1/merchants/#{merchant_id_one}/favorite_customer")
     favorite_customer_two = load_data("/api/v1/merchants/#{merchant_id_two}/favorite_customer")
 
-    assert_equal 1000, favorite_customer_one["id"]
-    assert_equal 118,  favorite_customer_two["id"]
+    assert_equal 1000,               favorite_customer_one["id"]
+    assert_equal_to_either 118, 458, favorite_customer_two["id"]
   end
 
   def test_loads_the_customers_with_pending_invoices_associated_with_one_merchant
